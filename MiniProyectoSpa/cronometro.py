1fe2c3c67d50023833d14fc6a4862a199a439480
EliasZerpa <93239378+EliasZerpa@users.noreply.github.com>
2024-08-21 12:56:17 -0300

    Add files via upload

---
+++ MiniProyectoSpa/cronometro.py
@@ -0,0 +1,38 @@
+gimpoert time
+
+def cronometro():
+    minutos = 0
+    segundos = 0
+
+    try:
+        while True:
+            print(f"{minutos:02}:{segundos:02}")  # Formato mm:ss
+            time.sleep(1)  # Espera 1 segundo
+            segundos += 1
+
+            if segundos == 60:  # Si los segundos llegan a 60, reinicia y suma un minuto
+                segundos = 0
+                minutos += 1
+
+    except KeyboardInterrupt:
+        print("\nCronómetro detenido.")
+
+# Ejecutar el cronómetro
+cronometro()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
