e3d0c3d0f693c5b4f21f8affcec9796649f43d45
VictorVagabculov <vvagabculov@gmail.com>
2024-08-23 09:49:34 -0300

    Primera prueba barra de servicios

--- MiniProyectoSpa/Recepcion_spa.py
+++ MiniProyectoSpa/Recepcion_spa.py
@@ -46,6 +46,31 @@ def eliminar_tarea():
     if seleccion:
         lista_cliente.delete(seleccion)
 
+
+barra_menu = tk.Menu(ventana)
+ventana.config(menu=barra_menu)
+menu_principal = tk.Menu(barra_menu)
+barra_menu.add_cascade(label =
+'Extras', menu=menu_principal)
+submenu0 = tk.Menu(menu_principal)
+menu_principal.add_cascade(label =
+'Masajista', menu=submenu0)
+submenu0.add_command(label = 'Femenino')
+submenu0.add_command(label = 'Masculino')
+submenu1 = tk.Menu(menu_principal)
+menu_principal.add_cascade(label =
+'Comida', menu=submenu1)
+submenu1.add_command(label = 'Barra de Cereal')
+submenu1.add_command(label =  'Mix frutos secos')
+submenu2 = tk.Menu(menu_principal)
+submenu2 = tk.Menu(menu_principal)
+menu_principal.add_cascade(label =
+'Bebida', menu=submenu2)
+submenu2.add_command(label = 'Jamaica')
+submenu2.add_command(label = 'Tamarindo')
+ventana.mainloop()
+
+
 boton_eliminar = tk.Button(ventana, text = '✖', command = eliminar_tarea,font=("Arial", 10), justify="center", background="lightblue", borderwidth=3,width=2,height=1)
 boton_eliminar.grid(row=2, column=1, columnspan=1, padx=10, pady=10)
 
