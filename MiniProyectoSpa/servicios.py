00c00865753d4ca7b014de3cb71083d069622752
VictorVagabculov <vvagabculov@gmail.com>
2024-09-06 14:48:51 -0300

    reparado error en servicios.

--- MiniProyectoSpa/servicios.py
+++ MiniProyectoSpa/servicios.py
@@ -1,41 +1,32 @@
+Manos = {
+    "Manicura Clásica": 45,
+    "Manicura con Gel": 60,
+    "Manicura Francesa": 50,
+    "Spa de Manos": 75,
+    "Aplicación de Uñas Acrílicas": 120
+}
 
+Pies = {
+    "Pedicura Clásica": 60,
+    "Pedicura Spa": 90,
+    "Pedicura con Gel": 90,
+    "Pedicura Detox": 75,
+    "Tratamiento para Callosidades": 45
+}
 
+Cutis = {
+    "Limpieza Facial Profunda": 60,
+    "Microdermoabrasión": 45,
+    "Tratamiento Facial Antiedad": 45,
+    "Peeling Químico": 30,
+    "Mascarilla Hidratante y Revitalizante": 30
+}
 
-Manos = [
-    "Manicura Clásica  45",
-    "Manicura con Gel 60",
-    "Manicura Francesa 50",
-    "Spa de Manos 75",
-    "Aplicación de Uñas Acrílicas 120"
-]
-
-Pies = [
-    "Pedicura Clásica 60",
-    "Pedicura Spa 90",
-    "Pedicura con Gel 90",
-    "Pedicura Detox 75",
-    "Tratamiento para Callosidades 45"
-]
-
-Cutis = [
-    "Limpieza Facial Profunda 60",
-    "Microdermoabrasión 45",
-    "Tratamiento Facial Antiedad 45",
-    "Peeling Químico 30",
-    "Mascarilla Hidratante y Revitalizante 30"
-]
-
-Masajes = ["Masaje Relajante 60",
-    "Drenaje Lifático 40",
-    "Masaje de Tejido Profundo 60",
-    "Masaje con Piedras Calientes 60",
-    "Masaje Tailandés 90",
-    "Masaje Aromaterapia 60",
-    "Masaje Relajante 60",
-    "Drenaje Lifático 45",
-    "Masaje de Tejido Profundo 60",
-    "Masaje con Piedras Calientes 60",
-    "Masaje Tailandés 90",
-    "Masaje Aromaterapia: 60 "]
-
-
+Masajes = {
+    "Masaje Relajante": 60,
+    "Drenaje Lifático": 40,
+    "Masaje de Tejido Profundo": 60,
+    "Masaje con Piedras Calientes": 60,
+    "Masaje Tailandés": 90,
+    "Masaje Aromaterapia": 60
+}
