f6b7e58576b19d66ff8c407768cec5decacdda3a
VicToR3212 <53920554+VicToR3212@users.noreply.github.com>
2024-09-07 12:51:55 -0300

    midificacionDeListaBotonservicios

--- MiniProyectoSpa/Recepcion_spa.py
+++ MiniProyectoSpa/Recepcion_spa.py
@@ -3,8 +3,6 @@ from tkinter import PhotoImage
 #from PIL import ImageTk, Image
 from  tkinter import  ttk,Frame
 import time
-
-
 from servicios import *
 
 # ------------------- ventana-----------
@@ -12,7 +10,7 @@ ventana = tk.Tk()
 ventana.title("Recepción Spa")
 ventana.geometry("1000x800")
 ventana.configure(background="lightblue")
-
+hola_servicio=""
 
 
 
@@ -47,28 +45,19 @@ frame2.pack(expand=True,fill="both")
 #va eso   image=logo_fondo,
 
 #---------------------------VARIABLES------------------------------------
+servicios = {'Manos': Manos, 'Pies': Pies, 'Cutis': Cutis, 'Masajes': Masajes}
 
 #--------------------------------logica -----------------------------------
 #----------for anidado----------------------------------------------------
 #----------Agregar-------------------
 
-def guardado_datos_clientes():
-    nombra="| "+manos.get()+" | "+pies.get()+" | "+cutis.get()+" | "+masaje.get()+"| "
-    but =nombre_cliente.get()
-    buta=""
-    vari=(but,nombra, buta)
-    nombre_cliente.set("")
-    manos.set("")
-    pies.set("")
-    cutis.set("")
-    masaje.set("")
-    return vari
 #-----------------AGREAGAR LISTA-----------------------------
 
-def agregar():
+#------------------------------agregar cliente ----------------------------
 
-    tablaa.insert("","0", values=(guardado_datos_clientes()))
-
+def agregarCliente():
+    labe.config(text=nombre_cliente.get())
+    nombre_cliente.set("")
 #---------------------------------eliminar-------------------------------------
 def eliminar_tarea():
     varSeleccionado = tablaa.selection()
@@ -83,6 +72,8 @@ def hora():
     reloj.config(text=tiempo_actual)
     ventana.after(1000, hora)
 
+#-------------------------||||||||||||||||------------------------------------
+
 #***************************************************************************
 
 #-------------MENU----------------------------------------------------------
@@ -91,10 +82,17 @@ def hora():
 
 #----------------BOTONES ------------------------------------------------------
 
+
+# boton salir
+boton_salir = tk.Button(frame3, text="salir", command=quit)
+boton_salir.grid(row=1, column=4, columnspan=1, padx=300, pady=10)
+# codigo Noemi
+
 #----------------agregar-------------------------------------------------------
-boton_agregar = tk.Button(frame3, text="✔", command=agregar, font=("Arial", 10),
+boton_agregar = tk.Button(frame3, text="✔", command=agregarCliente, font=("Arial", 10),
                           background="lightblue", borderwidth=3, width=2, height=1)
 boton_agregar.grid(row=3, column=0, columnspan=1, padx=10, pady=10)
+#---------------agregar cliente---------------------------------------------------
 
 #----------------eliminar-----------------------------------------------------
 boton_eliminar = tk.Button(frame2, text="✖", command=eliminar_tarea, font=("Arial", 10),
@@ -102,66 +100,29 @@ boton_eliminar = tk.Button(frame2, text="✖", command=eliminar_tarea, font=("Ar
 boton_eliminar.grid(row=6, column=0, columnspan=1, padx=10, pady=10)
 
 #***********************************************************************************************
-
+labe=tk.Label(frame3,font="consolas 15 bold",width=30)
+labe.grid(row=4, column=0)
 #----------------menu--------------------------------------------------------
 boton_menu = tk.Menubutton(frame3, text='Servicios Principales', relief=tk.RAISED, width=30, height=1,
                            font=("Arial", 14, "bold"), justify="center", background="lightblue", borderwidth=3)
-boton_menu.grid(row=0, column=0,)
-#---------------menu extras----------------------------------------------------
-extras_menu = tk.Menubutton(frame3, text='extras', relief=tk.RAISED, width=30, height=1,
-                           font=("Arial", 14, "bold"), justify="center", background="lightblue", borderwidth=3)
-extras_menu.grid(row=2, column=0, padx=10, pady=10)
+boton_menu.grid(row=1, column=0, padx=20, pady=20)
 
-#********************************************************************************************************************
+menu_principal = tk.Menu(boton_menu, tearoff=0, relief=tk.RAISED, font=("Arial", 14, "bold"), background="lightblue",
+                         borderwidth=3)
+boton_menu.config(menu=menu_principal)
 
-#------------------ cajas -------------------------------------------------------
-manos=ttk.Combobox(frame3,values=Manos,state="readonly")
-manos.grid(row=0,column=1)
-#------------------  -------------------------------------------------------
-pies=ttk.Combobox(frame3,values=Pies,state="readonly")
-pies.grid(row=0,column=2)
-#------------------  -------------------------------------------------------
-cutis=ttk.Combobox(frame3,values=Cutis,state="readonly")
-cutis.grid(row=0,column=3)
-#------------------  -------------------------------------------------------
-masaje=ttk.Combobox(frame3,values=Masajes,state="readonly")
-masaje.grid(row=0,column=4)
-#------------------------------------------------------------------------------
-"""
-codigo a restructurar 
-"""
-barra_menu = tk.Menu(ventana )
-ventana.config(menu=barra_menu)
-menu_principal = tk.Menu(barra_menu)
-barra_menu.add_cascade(label ='Extras', menu=menu_principal)
-submenu0 = tk.Menu(menu_principal)
-menu_principal.add_cascade(label ='Masajista', menu=submenu0)
-submenu0.add_command(label = 'Femenino')
-submenu0.add_command(label = 'Masculino')
+for categoria, opciones in servicios.items():
+    submenu = tk.Menu(menu_principal, tearoff=0, relief=tk.RAISED, font=("Arial", 14, "bold"), background="lightblue",
+                      borderwidth=3)
+    menu_principal.add_cascade(label=categoria, menu=submenu, )
 
-submenu1 = tk.Menu(menu_principal)
-menu_principal.add_cascade(label ='Comida', menu=submenu1)
-submenu1.add_command(label = 'Barra de Cereal')
-submenu1.add_command(label =  'Mix frutos secos')
-submenu2 = tk.Menu(menu_principal)
-submenu2 = tk.Menu(menu_principal)
-menu_principal.add_cascade(label =
-'Bebida', menu=submenu2)
-submenu2.add_command(label = 'Jamaica')
-submenu2.add_command(label = 'Tamarindo')
-#--------------------------------------------------------------------------------------
+    for llave, valor in opciones.items():
+        submenu.add_command(label=f"{llave}:{valor} minutos.",command=lambda valor=llave:tablaa.insert("","0", values=(valor,"")))
 
-
-
-#------------------------------------------------------------------------
-
-
-#-----------------------------------------------------------------------------
 #--------------Tabla----------------------------------------------------------
-tablaa=ttk.Treeview(frame2,columns=("co1","co2","co3"),show="headings")
-tablaa.grid(row=0,column=0)
+tablaa=ttk.Treeview(frame2,columns=("co2","co3"),show="headings")
+tablaa.grid(row=1,column=0)
 tablaa.heading("co3",text="extras")
-tablaa.heading("co1",text="nombre")
 tablaa.heading("co2",text="servicios")
 
 #-----------------nombre-------------------------------------------------------------
@@ -181,11 +142,8 @@ hora()
 #---------Scroll var---------
 barra_despalce=ttk.Scrollbar(frame2,orient="vertical",command=tablaa.yview)
 tablaa.config(yscrollcommand=barra_despalce.set)
-barra_despalce.grid(row=0,column=2,sticky="ns")
+barra_despalce.grid(row=1,column=2,sticky="ns")
 barra_despalce.config(command=tablaa.yview)
-
-
-
 
 
 ventana.mainloop()
