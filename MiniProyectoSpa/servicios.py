01559977da9f5c8a0920e13734b8462730a5b8c6
victor snaider <sanchon1945@gmail.com>
2024-09-10 12:52:57 -0300

    serviccios

---
+++ MiniProyectoSpa/servicios.py
@@ -0,0 +1,41 @@
+
+
+
+Manos = {
+    "Manicura Clásica  45":45,
+    "Manicura con Gel 60":60,
+    "Manicura Francesa 50":50,
+    "Spa de Manos 75":75,
+    "Aplicación de Uñas Acrílicas 120":120
+}
+
+Pies = {
+    "Pedicura Clásica 60":60,
+    "Pedicura Spa 90":90,
+    "Pedicura con Gel 90":90,
+    "Pedicura Detox 75":75,
+    "Tratamiento para Callosidades 45":45
+}
+
+Cutis = {
+    "Limpieza Facial Profunda 60":60,
+    "Microdermoabrasión 45":45,
+    "Tratamiento Facial Antiedad 45":45,
+    "Peeling Químico 30":30,
+    "Mascarilla Hidratante y Revitalizante 30":30
+}
+
+Masajes = {"Masaje Relajante 60":60,
+    "Drenaje Lifático 40":40,
+    "Masaje de Tejido Profundo 60":60,
+    "Masaje con Piedras Calientes 60":60,
+    "Masaje Tailandés 90":90,
+    "Masaje Aromaterapia 60":60,
+    "Masaje Relajante 60":60,
+    "Drenaje Lifático 45":45,
+    "Masaje de Tejido Profundo 60":60,
+    "Masaje con Piedras Calientes 60":60,
+    "Masaje Tailandés 90":90,
+    "Masaje Aromaterapia: 60 ":60}
+
+
